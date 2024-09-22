d8b5fb439240e45534da7d2e55f33047469a5c9b
Jafarscript <lihammedjafar@gmail.com>
2024-09-22 17:21:29 +0100

    Updated the db url

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,5 +1,9 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema'
-const sql = neon("postgresql://final_year_owner:3JiCmVb1Ecdu@ep-still-sound-a5eheppz.us-east-2.aws.neon.tech/final_year?sslmode=require");
+import * as schema from './schema';
+
+const databaseUrl = process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL;
+
+const sql = neon(databaseUrl);
 export const db = drizzle(sql, {schema});
+
